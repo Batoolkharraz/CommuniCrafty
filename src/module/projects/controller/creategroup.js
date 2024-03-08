e4a5449acee1cba0b0782c5fe13292e7558f458d
Batoolkharraz <99608072+Batoolkharraz@users.noreply.github.com>
2024-03-09 01:27:39 +0200

    group fix

--- src/module/projects/controller/creategroup.js
+++ src/module/projects/controller/creategroup.js
@@ -1,73 +1,86 @@
 import projectGroupModel from "../../../../DB/model/group.model.js";
 import userModel from "../../../../DB/model/user.model.js";
 import { asyncHandler } from "../../../Services/errorHandling.js";
-import { generateToken } from "../../../Services/generateAndVerify.js";
+import { generateToken, verifyToken } from "../../../Services/generateAndVerify.js";
 import { sendEmail } from "../../../Services/sendEmail.js";
 
-export const getproject=asyncHandler(async(req,res,next)=>{
-    const email=req.body.email
-  
-     const user = await userModel.findById(req.user._id);
+export const createGroupProject = asyncHandler(async (req, res, next) => {
+    const { projectname, description, members } = req.body;
+
+    const user = await userModel.findById(req.user._id);
     if (!user) {
         return next(new Error(`please sign up first `, { cause: 400 }));
     }
-  console.log(req.headers.host)
-  const member = await userModel.findOne({ email: email });
-  const userid = member._id.toString();
-   console.log(userid)
-  const newproject=await projectGroupModel.create(
-        {
-            projectname: req.body.projectname,
-            description:req.body.description,
-            members:[
-                {
-                    userId:userid,
-                    task:req.body.task,
-                }
-            ]
+
+    console.log(req.headers.host);
+
+    const membersData = [];
+    for (const memberData of members) {
+        const { email, task } = memberData;
+        const member = await userModel.findOne({ email: email });
+        if (!member) {
+            // If any member doesn't exist, you can handle this error as needed
+            return next(new Error(`User with email ${email} not found`, { cause: 404 }));
         }
-  )
-  const projid=newproject._id;
- let token =generateToken({email,projid},process.env.SIGNUP_TOKEN,60*5);
- const refreshToken = generateToken({email,projid},process.env.SIGNUP_TOKEN,60*60*24)
+        membersData.push({
+            userId: member._id.toString(),
+            task: task,
+        });
+    }
 
-const link =`${req.protocol}://${req.headers.host}/auth/confirmEmail/${token}`;
+    const newProject = await projectGroupModel.create({
+        projectname: projectname,
+        description: description,
+        members: membersData,
+    });
 
-const Rlink =`${req.protocol}://${req.headers.host}/auth/newConfirmEmail/${refreshToken}`;
+    const projid = newProject._id;
+   
+    // Sending email to each member individually, adjust as needed
+    for (const memberData of members) {
+        let email = memberData.email;
+        const token = generateToken({ email, projid }, process.env.SIGNUP_TOKEN, 60 * 5);
+        const refreshToken = generateToken({ email, projid }, process.env.SIGNUP_TOKEN, 60 * 60 * 24);
 
-  const html = `<a href="${link}">Accept invitation</a>  <br/> <br/> <br/> <a href="${Rlink}"> send new email </a> `
-  await sendEmail(email,'confirm email',html)
-  console.log(projid)
-  return res.status(200).json({ message: "Project deleted group created", projid });
+        const link = `${req.protocol}://${req.headers.host}/project/confirmEmail/${token}`;
+        const Rlink = `${req.protocol}://${req.headers.host}/project/newConfirmEmail/${refreshToken}`;
+        const html = `<a href="${link}">Accept invitation</a>  <br/> <br/> <br/> <a href="${Rlink}"> send new email </a> `;
+   
+        await sendEmail(memberData.email, 'confirm email', html);
+    }
+
+    console.log(projid);
+    return res.status(200).json({ message: "Project deleted group created", projid });
+});
+
+
+
+export const confirmEmail = asyncHandler(async (req, res) => {
+
+    const { token } = req.params;
+    const decoded = verifyToken(token, process.env.SIGNUP_TOKEN);
+    const projid = decoded.projid;
+    const user = await userModel.findOne({ email: decoded.email })
+    const proj = await projectGroupModel.findById(projid)
+    console.log(user);
+    if (!decoded?.email || !projid) {
+        return next(new Error("invalid token payload", { cause: 409 }));
+    }
+    if (!proj) {
+        return next(new Error(`No projects added`));
+    }
+    let updated = false;
+    for (let i = 0; i < proj.members.length; i++) {
+        if (proj.members[i].userId.toString() === user._id.toString()) {
+            proj.members[i].confirm = true;
+            await proj.save();
+            updated = true;
+            break;
+        }
+    }
+    if (updated) {
+        return res.status(200).redirect(`${process.env.URL}`);
+    } else {
+        return next(new Error("not register account or ur email is verify", { cause: 400 }))
+    }
 })
-
-
-
-export const confirmEmail = async(req,res)=>{
-    const {token}=req.params;
-    const decoded=verifyToken(token,process.env.SIGNUP_TOKEN);
-    const projid=decoded.projid;
-    const user=await userModel.findOne({email:decoded.email})
-    if(!decoded?.email|| !projid){
-        return next(new Error("invalid token payload",{cause:409}));
-    }
-const proj=await projectGroupModel.findById(projid)
-if(!proj)
-{
-    return next(new Error(`No projects added`));
-}
-let updated = false;
-for (const member of proj.members) {
-    if (member.userId.toString() === user._id.toString()) {
-        await projectGroupModel.updateOne({ userId:user._id, confirm: true });
-        updated = true;
-                break;
-    }
-}
-if(updated){
-    return res.status(200).redirect(`${process.env.URL}`);
-}
-else{
-    return next(new Error("not register account or ur email is verify",{cause:400}))
-}
-}
--- src/module/projects/projects.router.js
+++ src/module/projects/projects.router.js
@@ -3,7 +3,7 @@ import fileUpload, { fileValidation } from "../../Services/multer_cloud.js";
 import * as projectcont from "../../module/projects/controller/projects.contorl.js"
 import auth, { roles } from "../../MiddleWare/auth.middleware.js";
 import { endPointproject } from './end.js';
-import { getproject,confirmEmail } from "./controller/creategroup.js";
+import { createGroupProject,confirmEmail } from "./controller/creategroup.js";
 import { validation } from "../../MiddleWare/validation.js";
 import * as validators from '../auth/auth.validation.js';
 // import { validation } from "../../MiddleWare/validation.js";
@@ -14,7 +14,7 @@ projectrouter.delete('/delete',auth(endPointproject.delete),projectcont.deletePr
 projectrouter.patch('/update/:id',auth(endPointproject.update),fileUpload(fileValidation.image).single('image'),projectcont.updateProject)
 projectrouter.get('/get',auth(endPointproject.get),projectcont.getproject);
 projectrouter.get('/search',auth(endPointproject.search),projectcont.serach);
-projectrouter.get('/Group',auth(endPointproject.create),getproject);
+projectrouter.get('/Group',auth(endPointproject.create),createGroupProject);
 projectrouter.get('/confirmEmail/:token',validation(validators.token),confirmEmail);
 
 export default projectrouter;
