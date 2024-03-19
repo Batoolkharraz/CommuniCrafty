c4fccfbadece88373590e765bd8f09062493a2d9
Alaa Adham <103996515+AlaaAdham@users.noreply.github.com>
2024-03-19 15:08:36 +0200

    up

--- src/module/Group/end.js
+++ src/module/Group/end.js
@@ -1,11 +1,11 @@
 import { roles } from "../../MiddleWare/auth.middleware.js";
 
 export const endPointproject = {
-  add: [roles.user],
-  delete: [roles.user],
+  add: [roles.admin,roles.user],
+  delete: [roles.admin,roles.user],
   update: [roles.user, roles.admin],
   get: [roles.admin,roles.user],
-  search: [roles.admin, roles.user],
-  create: [roles.admin, roles.user],
+  search:[roles.admin, roles.user],
+  create:[roles.admin, roles.user],
   deleteuser:[roles.admin,roles.user]
 }
